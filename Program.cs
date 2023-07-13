8b11dfd230ede1656e09c22b85cc7336d75e82f8
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-07-13 14:23:32 +0530

    day 4

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D4A4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+           number= int.Parse(Console.ReadLine());
+            if (number >0)
+            {
+                Console.WriteLine("number is positive");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("number is zero");
+            }
+            else
+            {
+                Console.WriteLine("number is negative");
+            }
+            var result=number%2==0 ? "number is even " : "number is odd";
+            Console.WriteLine(result);
+        }
+    }
+}
